efdb5784c89d06c7a60b7ce0aa7751ba8e41a21b
Jude Fiorillo <jude.fiorillo@gmail.com>
2014-11-12 16:57:24 -0500

    create update, edit, and destroy actions for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,12 +21,22 @@ class UsersController < ApplicationController
 	end
 
 	def edit
+		@user = User.find(params[:id])
 	end
 
 	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(user_params)
+			redirect_to users_path
+		else
+			render :new
+		end
 	end
 
 	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		redirect_to users_path
 	end
 
 	private
