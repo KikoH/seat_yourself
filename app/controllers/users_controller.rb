fc554e200d9f137a598ac2bb8e2f26b45044e7b6
Jude Fiorillo <jude.fiorillo@gmail.com>
2014-11-12 16:41:26 -0500

    create index, new, show, and create actions for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 	def index
-		@user = User.all
+		@users = User.all
 	end
 
 	def new
@@ -12,6 +12,12 @@ class UsersController < ApplicationController
 	end
 
 	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to users_path
+		else
+			render :new
+		end
 	end
 
 	def edit
@@ -23,5 +29,9 @@ class UsersController < ApplicationController
 	def destroy
 	end
 
+	private
+	def user_params
+		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	end
 
 end
