baefd5853d4dfd42b1e16e085e6cf8de83d4d9be
Jude Fiorillo <jude.fiorillo@gmail.com>
2014-11-13 16:19:39 -0500

    Create actions for restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -21,15 +21,28 @@ class RestaurantsController < ApplicationController
 		end
 	end
 
+	def edit
+		@restaurant = Restaurant.find(params[:id])
+	end
+
 	def update
+		@restaurant = Restaurant.find(params[:id])
+		if @restaurant.update_attributes(restaurant_params)
+			redirect_to restaurants_path
+		else
+			render :edit
+		end
 	end
 
 	def destroy
+		@restaurant = Restaurant.find(params[:id])
+		@restaurant.destroy
+		redirect_to restaurants_path
 	end
 
 	private
 	def restaurant_params
-		params.require(:restaurant).permit(:name, :location)
+		params.require(:restaurant).permit(:name, :location, :owner_id)
 	end
 
 
