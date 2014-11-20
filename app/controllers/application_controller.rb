52c4f362ca683e8b961228704223c64d2ace8802
Kiko <mmhabib11@gmail.com>
2014-11-20 18:57:46 -0500

    Add methods for current user and to ensure owner logged in for restaurant creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   private
+  
   def current_user
   	@current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-
   helper_method :current_user
+  
+  def ensure_owner
+   	if !current_user || (current_user && !current_user.type == "Owner")
+  		redirect_to new_session_path
+  	end
+  end
+
+  
 end
