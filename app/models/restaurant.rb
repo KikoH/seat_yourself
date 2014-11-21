c2d5b10f8bc523863d9d260056ba55a94a036cc1
Kiko <mmhabib11@gmail.com>
2014-11-20 19:01:38 -0500

    Check to see if restaurant is available at a specific time and if there is space available

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,6 +4,7 @@ class Restaurant < ActiveRecord::Base
 	belongs_to :owner
 
 	def available?(party_size, book_time)
+		# 2:21 vs 2:00
 		reserved = reservations.where(:book_time => book_time).sum(:party_size)
 		if (party_size <= (capacity - reserved)) && (book_time.hour.between?(open_time.hour, close_time.hour))
 			return true
